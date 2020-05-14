5714b98ac1cb87a701265eca126083e47e267a46
aditya <adityavadityav@gmail.com>
2020-05-14 10:40:58 +0530

    Created Structure for pages

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import NavLink from "./NavLink";
-import {useDispatch, useSelector} from "react-redux";
-import {LOGOUT, TOGGLE_LOGIN, TOGGLE_SIGNUP, TOGGLE_UPLOAD} from "../../../actions/types";
-import './style.css'
+import { useDispatch, useSelector } from "react-redux";
+import { LOGOUT, TOGGLE_LOGIN, TOGGLE_SIGNUP, TOGGLE_UPLOAD } from "../../../actions/types";
+import './style.scss'
 
 const Header = () => {
     const dispatch = useDispatch();
-    const {user} = useSelector(state => state.auth)
+    const { user } = useSelector(state => state.auth)
     const openLogin = () => {
-        dispatch({type: TOGGLE_LOGIN})
+        dispatch({ type: TOGGLE_LOGIN })
     }
     const openSignUp = () => {
-        dispatch({type: TOGGLE_SIGNUP})
+        dispatch({ type: TOGGLE_SIGNUP })
     }
     const openUpload = () => {
-        dispatch({type: TOGGLE_UPLOAD})
+        dispatch({ type: TOGGLE_UPLOAD })
     }
     const routes = [{
         route: '/photo',
@@ -43,7 +43,7 @@ const Header = () => {
         action: () => openUpload(),
         label: 'Upload'
     }, {
-        action: () => dispatch({type: LOGOUT}),
+        action: () => dispatch({ type: LOGOUT }),
         label: 'Logout'
     }]
 
@@ -53,7 +53,7 @@ const Header = () => {
             return privateRoute.map(route => (
                 <li className="nav-item hover" onClick={route.action}>
                     <a className="nav-link">{route.label}</a>
-                </li>
+                </li> 
             ))
         } else {
             return publicRoute.map(route => (
@@ -66,33 +66,87 @@ const Header = () => {
 
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <NavLink to="/">
-                <p className="navbar-brand">FLICAF</p>
-            </NavLink>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    {routes.map(route => (
-                        <NavLink to={route.route}>
-                            <li className="nav-item ">
-                                <p className="nav-link" href="#">{route.label}</p>
-                            </li>
+        <div>
+            <header>
+                <div className="headerLeftSection">
+                    <div className="headerLogoSection">
+                        <NavLink to="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="main_logo" viewBox="0 0 594 171">
+                                <g id="Group_242" data-name="Group 242" transform="translate(-11958 1551)">
+                                    <rect id="Rectangle_736" data-name="Rectangle 736" width="48" height="24"
+                                        transform="translate(12037 -1477)" fill="#272727" />
+                                    <rect id="Rectangle_738" data-name="Rectangle 738" width="72" height="24"
+                                        transform="translate(12037 -1508)" fill="#272727" />
+                                    <rect id="Rectangle_737" data-name="Rectangle 737" width="24" height="24"
+                                        transform="translate(12037 -1446)" fill="#272727" />
+                                    <rect id="Rectangle_739" data-name="Rectangle 739" width="24" height="24"
+                                        transform="translate(12006 -1446)" fill="#1f67e3" />
+                                    <rect id="Rectangle_740" data-name="Rectangle 740" width="24" height="24"
+                                        transform="translate(11982 -1477)" fill="#fbb03b" />
+                                    <rect id="Rectangle_741" data-name="Rectangle 741" width="24" height="24"
+                                        transform="translate(11958 -1508)" fill="#d95e50" />
+                                    <text id="FLICAF" transform="translate(12143 -1423)" fill="#272727" fontSize="122"
+                                        fontFamily="Poppins-Bold, Poppins" fontWeight="700">
+                                        <tspan x="0" y="0">FLICAF</tspan>
+                                    </text>
+                                </g>
+                            </svg>
                         </NavLink>
-                    ))}
-                </ul>
-                <ul className="navbar-nav ml-auto">
-                    <li className={'nav-item'}><a className="nav-link">{user && user.name}</a></li>
-                    {actionRoute()}
 
-                </ul>
+                    </div>
+
+                    <div className="headerNavWrap">
+                        <ul>
+                            {routes.map(route => (
+                                <NavLink to={route.route}>
+                                    <li >
+                                        <span>{route.label}</span>
+                                    </li>
+                                </NavLink>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+       
+
+                <div className="headerRightSection">
+                    <div className="search_wrap">
+                        <button className="btn btn-default">
+                            <img src="/assets/images/portalicons/search.svg" className="headerIcon" />
+                        </button>
+                    </div>
+                    <div className="categoryWrap">
+                        <div className="dropdown">
+                            <button type="button" className="btn btn-default dropdown-toggle category" data-toggle="dropdown">Category <img src="/assets/images/portalicons/arrow.svg" className="headerIcon" /></button>
+                          
+                        </div>
+                    </div>
+                    <div className="profileWrap">
+                        <img src="/assets/images/portalicons/profile.jpg" className="img-fluid" />
+                       {user && user.name}
+                        {actionRoute()} - ithentha
+                    </div>
+                    <div className="notification_wrap">
+                        <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                            <img src="/assets/images/portalicons/notification.svg" className="headerIcon" />
+                        </button>
+                       
+                    </div>
+                    <div className="accountSettings_wrap">
+                        <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                            <img src="/assets/images/portalicons/settings.svg" className="headerIcon" />
+                        </button>
+                      
+                    </div>
+                </div>
+            </header>            
             </div>
-        </nav>);
+            );
+
+
 };
 
 export default Header;
+
+
+
