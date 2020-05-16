5d7851285389863d42aa27cff1a540efd26c11d1
aditya <adityavadityav@gmail.com>
2020-05-17 00:43:12 +0530

    Removed all files

--- README.md
+++
@@ -1,9 +0,0 @@
-#React Web App For https://flicaf.com/
-
-```bash
-# Install dependencies
-npm install
-
-# Serve on localhost:3000
-npm start
-```
--- src/actions/photoActions.js
+++
@@ -1,6 +0,0 @@
-import {GET_PHOTO} from "./types";
-import {IMAGES} from "../Constants/data";
-
-export const getImages = () => dispatch => {
-    dispatch({type: GET_PHOTO, payload: IMAGES})
-}
