7c733bcb19387ae9a5e25fa017b2d722b836aba8
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-12-01 16:29:51 -0600

    fix: issue with store on testing

--- src/store.js
+++ src/store.js
@@ -1,17 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+
 import rootReducer from './reducers';
 
 const initialState = {};
-
-const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk),
   )
 );
 
