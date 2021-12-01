304f2a622de838c998f5ea712f99771ea6b5d380
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-12-01 16:50:34 -0600

    refactor: replace `connect` with `useDispatch` and `useSelector`

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,44 +1,34 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { fetchPosts } from '../actions/postActions';
 
-class Posts extends Component {
-  componentDidMount() {
-    this.props.fetchPosts();
-  }
+function Posts() {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.items);
+  const newPost = useSelector((state) => state.posts.item);
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.newPost) {
-      this.props.posts.unshift(nextProps.newPost);
-    }
-  }
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
 
-  render() {
-    const postItems = this.props.posts.map(post => (
-      <div key={post.id}>
-        <h3>{post.title}</h3>
-        <p>{post.body}</p>
-      </div>
-    ));
-    return (
-      <div>
-        <h1>Posts</h1>
-        {postItems}
-      </div>
-    );
-  }
+  const postItems = useMemo(
+    () =>
+      (newPost ? [newPost, ...posts] : posts).map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      )),
+    [posts, newPost],
+  );
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {postItems}
+    </div>
+  );
 }
 
-Posts.propTypes = {
-  fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired,
-  newPost: PropTypes.object
-};
-
-const mapStateToProps = state => ({
-  posts: state.posts.items,
-  newPost: state.posts.item
-});
-
-export default connect(mapStateToProps, { fetchPosts })(Posts);
+export default Posts;
